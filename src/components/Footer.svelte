ebe7118c0214c11f5a83a69b53b5949257bcc7eb
saiphp <besabellacyrus@gmail.com>
2020-06-14 20:19:54 +0800

    done footer instagram layout

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,34 @@
+<div class="spacer"></div>
 <footer>
-  <div class="text-center">
-    <p style="text-align:center;white-space:pre-wrap;" class="">
-      © 2020 Hatch Essentials • Young Living Independent Distributor
-    </p>
+	<div class="container mx-auto">
+  <div><hr></div>
+  <section class="hatch-instagram"> 
+    <div class="left-section">
+      <div class="hatch-logo">
+        <img src="logo.svg" />
+      </div>
+      <div>
+        <p>For daily updates, inspirations and anything about hatch essentials,
+        please follow us on instagram.</p>
+        <p>Follow us <b>@hatchessentials</b></p>
+      </div>
+    </div>
+    <div class="instagram-items">
+      <div>
+      	<img src="http://hatchessentials.com/wp-api/wp-content/uploads/2020/06/Pursue-Dreams.jpg" alt="">
+      </div>
+      <div class="">
+      	<img src="http://hatchessentials.com/wp-api/wp-content/uploads/2020/06/Pursue-Dreams.jpg" alt="">
+      </div>
+      <div class="">
+      	<img src="http://hatchessentials.com/wp-api/wp-content/uploads/2020/06/Pursue-Dreams.jpg" alt="">
+      </div>
+    </div>
+  </section>
+</div>
+  <div class="container mx-auto">
+    <span style="font-size: 0.7rem; white-space:pre-wrap;">
+      Copyright © 2020 Hatch Essentials
+    </span>
   </div>
 </footer>
